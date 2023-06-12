3374f131724b5e580e8604b96c891908451b4fb2
Michael Hodge Jr <mikeh@lightsky.com>
2023-06-11 20:43:40 -0500

    Package Extension

--- scripts/background.js
+++ scripts/background.js
@@ -1,60 +1,23 @@
-
-const demo = "https://demo.shepherd.vet/";
-const production = "https://app.shepherd.vet/";
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.setBadgeText({
-    text: "OFF",
-  });
-});
-
-chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith(demo) || tab.url.startsWith(production)) {
-    // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    // Next state will always be the opposite
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON'
-
-    // Set the action badge to the next state
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === "notification") {
+    // Notify the user of the aler
+    chrome.notifications.create({
+      type: "basic",
+      iconUrl: "/icon.png",
+      title: "Shepherd Alert!",
+      message: "You have a new alert in Shepherd!",
     });
 
-    if (nextState === "ON") {
-      // Inject the content script when the extension is 'ON'
-      await chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ["scripts/notifier.js"],
-      });
-    } else {
-    }
+    playSound();
   }
 });
-
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.message === "notification"){
-      // Notify the user of the aler
-      chrome.notifications.create({
-        type: "basic",
-        iconUrl: "/icon.png",
-        title: "Shepherd Alert!",
-        message: "You have a new alert in Shepherd!",
-      });
-
-      playSound();
-    }
-
-  }
-);
 
 /**
  * Plays audio files from extension service workers
  * @param {string} source - path of the audio file
  * @param {number} volume - volume of the playback
  */
-async function playSound(source = 'ding.mp3', volume = 1) {
+async function playSound(source = "ding.mp3", volume = 1) {
   await createOffscreen();
   await chrome.runtime.sendMessage({ play: { source, volume } });
 }
@@ -63,8 +26,8 @@ async function playSound(source = 'ding.mp3', volume = 1) {
 async function createOffscreen() {
   if (await chrome.offscreen.hasDocument()) return;
   await chrome.offscreen.createDocument({
-      url: 'offscreen.html',
-      reasons: ['AUDIO_PLAYBACK'],
-      justification: 'testing' // details for using the API
+    url: "offscreen.html",
+    reasons: ["AUDIO_PLAYBACK"],
+    justification: "testing", // details for using the API
   });
 }
